85e6b95275fb9e89a2ebd5d1693e89ea5d0919e9
Muhammad Tehami <t3hami@gmail.com>
2019-10-09 19:01:23 +0500

    init

---
+++ config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    SENDGRID_APIKEY: "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"
+}
---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { SENDGRID_APIKEY } = require('./config');
+const sgMail = require('@sendgrid/mail');
+
+sgMail.setApiKey(SENDGRID_APIKEY);
+const app = express();
+app.use(bodyParser.json());
+
+app.post('/', (req, res) => {
+    const {from, name, text} = req.body;
+    const msg = {
+    to: 't3hami@gmail.com',
+    from: from,
+    subject: 'New Message On Website',
+    text: `From: ${name}\n Message: ${text}`,
+    html: `<h3>From: ${name}</h3></br><h3>Message:</h3></br><p>${text}</p>`,
+    };
+    sgMail.send(msg);
+    res.status(201).send()
+});
+
+app.listen(process.env.PORT, () => console.log('Express server is listening on port '+process.env.PORT));
